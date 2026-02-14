06e11e8d57aa3978c0803864f383e6510a29de44
mutayab <mutayabkhalid96@gmail.com>
2026-02-14 09:48:26 +0100

    webscarpper from Wikipedia

---
+++ tennis_grandslam_champs.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import time
+import datetime
+import requests
+
+
+
+url = "https://en.wikipedia.org/wiki/List_of_Grand_Slam_singles_champions_in_Open_Era_with_age_of_first_title"
+
+
+def extract(url: str) -> BeautifulSoup:
+    """
+    Fetch the content in the webpage
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, "html.parser")
+    print("Status-code:", r.status_code)
+    return soup
+
+
+def tranform(soup):
+    """
+    Extract the information contained in the page
+    """
+    men_div = soup.find_all("tr")
+    header = men_div.pop(0) #header of the first table
+    
+    table_men = []
+
+    for container in men_div:
+        cols = container.find_all("td")  #returns list of all columns
+        
+        if not cols:
+            break
+
+        entry = {
+            "age": cols[0].get_text(strip=True),
+            "name": cols[1].get_text(strip=True),
+            "first_tournament": cols[2].get_text(strip=True),
+            "first_title_date": cols[4].get_text(strip=True),
+        }
+        table_men.append(entry)
+
+    df = pd.DataFrame(table_men)
+    print(df) 
+    
+    return
+
+
+if __name__ == "__main__":
+    soup = extract(url)
+    tranform(soup)
